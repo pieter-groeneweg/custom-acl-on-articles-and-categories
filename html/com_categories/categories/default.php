5878f9da0143c0100525540e443c10b1efb2be0a
pieter-groeneweg <pieter@groenewegdigitaal.nl>
2020-09-30 10:48:37 +0200

    Overrides for j!3.9
    Overrides adjust for 3.9

--- html/com_categories/categories/default.php
+++ html/com_categories/categories/default.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Administrator
  * @subpackage  com_categories
  *
- * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -140,7 +140,7 @@ if ($saveOrder)
 						else {
 						$canChange  = $user->authorise('core.edit.state', 'com_content.article.' . $item->id) && $canCheckin;
 						}
-
+						
 						// Get the parents of item for sorting
 						if ($item->level > 1)
 						{
